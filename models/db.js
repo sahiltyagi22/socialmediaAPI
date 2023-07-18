3e5d023fa45faf7dd3bf21f9dedc1a9eb2ebbcae
sahil <sahiltyagi1204@outlook.com>
2023-07-18 22:06:08 +0530

    db file

---
+++ models/db.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username :{
+        type :String,
+        require : true,
+        min :3,
+        max :20,
+        unique : true
+    },
+
+    email :{
+        type :String,
+        require : true,
+        max :50,
+        unique :true
+    },
+
+    password : {
+        type : String,
+        require : true,
+        min :6
+    },
+
+    profilePicture :{
+        type : String,
+        default : ""
+    },
+
+    followers : {
+        type : Array,
+        default : []
+    },
+
+    followings : {
+        type : Array,
+        default : []
+    },
+
+    isAdmin : {
+        type : Boolean,
+        default : false
+    }
+
+},
+
+{timestamps :true}
+
+)
+
+
+module.exports = mongoose.model('Users' , userSchema)
