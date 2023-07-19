1b22c4164044dfe87f894034c4d5dac0454f05be
sahil <sahiltyagi1204@outlook.com>
2023-07-19 23:28:27 +0530

    hash password

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,31 @@
-const router = require('express').Router()
-const Users = require('../models/db')
+const router = require("express").Router();
+const express = require('express')
+const Users = require("../models/db");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+var bodyParser = require('body-parser');
 
-router.get('/register', async (req,res)=>{
-    const newUser = await new Users ({
-        name : "sahil Tyagi",
-        email : "sahiltyagi@121.com",
-        password : 12121212
-    })
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
-    await newUser.save()
-    res.send("ok")
-})
+router.post("/register", async (req, res) => {
+  try {
+    //  generating hash password
+    bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+        console.log(req.body.password);
+      const newUser = new Users({
+        username: req.body.username,
+        email: req.body.email,
+        password: hash,
+      });
+      // saving info
+      await newUser.save();
+      res.send("all okay");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-
-module.exports = router
+module.exports = router;
